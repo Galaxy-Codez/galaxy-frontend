ab07ddba6ee323647aff6a199672683f4cd55714
AfzalAli231 <abroimdad38@gmail.com>
2024-01-12 23:31:17 +0500

    remote blog sitemap for now

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,22 +1,22 @@
-import { Blog } from "@/types/blogs.interface";
+// import { Blog } from "@/types/blogs.interface";
 import { MetadataRoute } from "next";
 
 const BASE_URL = "https://galaxy-frontend.vercel.app";
-const API_BASE_URL = "http://localhost:8080";
+// const API_BASE_URL = "http://localhost:8080";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const blogs: Blog[] = await new Promise(function (resolve, reject) {
-    fetch(API_BASE_URL + "/blogs")
-      .then((res) => res.json())
-      .then((data) => resolve(data.data))
-    .catch((err) => reject(err));
-  });
-  const blogRoutes: MetadataRoute.Sitemap = blogs.map((blog: Blog) => ({
-    url: `${BASE_URL}/${blog.slug}`,
-    lastModified: new Date(blog.createdAt),
-    changeFrequency: "yearly",
-    priority: 1,
-  }));
+  // const blogs: Blog[] = await new Promise(function (resolve, reject) {
+  //   fetch(API_BASE_URL + "/blogs")
+  //     .then((res) => res.json())
+  //     .then((data) => resolve(data.data))
+  //   .catch((err) => reject(err));
+  // }).catch(() => []);
+  // const blogRoutes: MetadataRoute.Sitemap = blogs.map((blog: Blog) => ({
+  //   url: `${BASE_URL}/${blog.slug}`,
+  //   lastModified: new Date(blog.createdAt),
+  //   changeFrequency: "yearly",
+  //   priority: 1,
+  // }));
 
   return [
     {
@@ -31,6 +31,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: "yearly",
       priority: 1,
     },
-    ...blogRoutes,
+    // ...blogRoutes,
   ];
 }

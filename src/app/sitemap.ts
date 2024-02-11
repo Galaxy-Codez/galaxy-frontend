4e4154a2d9648104a3f2af2e914a3b8dbeae5c36
AfzalAli231 <abroimdad38@gmail.com>
2024-02-11 22:53:02 +0500

    update sitemap files

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,21 @@
+import { Blog } from "@/types/blogs.interface";
 import { MetadataRoute } from "next";
 
 const BASE_URL = process.env.FRONTEND_URL;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const blogs: Blog[] = await new Promise<Blog[]>(function (resolve, reject) {
+    fetch(process.env.BACKEND_URL + "/blogs")
+      .then((res) => res.json())
+      .then((data) => resolve(data.data))
+      .catch((err) => reject(err));
+  }).catch(() => []);
+  const blogRoutes: MetadataRoute.Sitemap = blogs.map((blog: Blog) => ({
+    url: `${BASE_URL}/blogs/${blog?.slug}`,
+    lastModified: new Date(blog?.created_at),
+    changeFrequency: "yearly",
+    priority: 1,
+  }));
   return [
     {
       url: BASE_URL,
@@ -11,10 +24,17 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 1,
     },
     {
-      url: `${BASE_URL}/blogs/sitemap.xml`,
+      url: BASE_URL + "/contact",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
+    {
+      url: `${BASE_URL}/blogs`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    ...blogRoutes,
   ];
 }
